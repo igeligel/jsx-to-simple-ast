3cb9c753f746813268816fd0ba5f29a41f55a340
igeligel <kevinigeligeligel@gmail.com>
2018-11-03 16:33:38 +0100

    add additional plugins

--- src/parser.js
+++ src/parser.js
@@ -2,6 +2,9 @@ const babelParser = require('@babel/parser');
 const fs = require('fs');
 
 var content = fs.readFileSync(__dirname + '/Welcome/index.js', 'utf8');
-const ast = babelParser.parse(content);
+const ast = babelParser.parse(content, {
+  sourceType: 'module',
+  plugins: ['jsx'],
+});
 
 console.log(ast);
