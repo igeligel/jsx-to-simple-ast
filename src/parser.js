857ba809d3bb7fdb810d0f9221c588739cb5b810
igeligel <kevinigeligeligel@gmail.com>
2018-11-10 14:54:03 +0100

    refactor parser file

--- src/parser.js
+++ src/parser.js
@@ -1,53 +1,35 @@
 const babelParser = require('@babel/parser');
 
+const reduceAst = (oldNode, currentNode) => {
+  let element = {};
+  if (currentNode.type === 'JSXElement') {
+    element = {
+      name: currentNode.openingElement.name.name,
+      children: [],
+    };
+    oldNode.push(element);
+  }
+  if ('children' in currentNode) {
+    currentNode.children.forEach(
+      (node) =>
+        oldNode.length > 0
+          ? reduceAst(element.children, node)
+          : reduceAst(oldNode, node),
+    );
+  }
+  return oldNode;
+};
+
 const getTree = (content) => {
   const ast = babelParser.parse(content, {
     sourceType: 'module',
     plugins: ['jsx'],
   });
-
-  const a = ast.program.body.find(
+  const initialAst = ast.program.body.find(
     (astNode) => astNode.type === 'ExportNamedDeclaration',
   ).declaration.declarations[0].init.body.body[0].argument;
 
-  function showStuff(oldNode, currentNode) {
-    let someThingPushed = false;
-
-    var element = {};
-    if (
-      currentNode &&
-      'type' in currentNode &&
-      currentNode.type === 'JSXElement' &&
-      oldNode
-    ) {
-      element = {
-        name: currentNode.openingElement.name.name,
-        children: [],
-      };
-      oldNode.push(element);
-      someThingPushed = true;
-    }
-    if (
-      currentNode &&
-      typeof currentNode === 'object' &&
-      'children' in currentNode &&
-      currentNode.children.length
-    ) {
-      currentNode.children.forEach((node) => {
-        if (someThingPushed) {
-          return showStuff(element.children, node);
-        } else {
-          return showStuff(oldNode, node);
-        }
-      });
-    } else {
-      return oldNode;
-    }
-  }
-
-  var newStuff = [];
-  showStuff(newStuff, a);
-  return newStuff;
+  return reduceAst([], initialAst);
 };
 
 module.exports = getTree;
